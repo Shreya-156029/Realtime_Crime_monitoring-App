562b0c9ba1c77ecef1d17697c4cb34f3449db489
SHREYA <skulkarni156029@gmail.com>
2020-02-16 08:58:48 +0530

    new commit

---
+++ app/src/main/java/com/example/reliance/real_time_crime_monitoring/recycler.java
@@ -0,0 +1,14 @@
+package com.example.reliance.real_time_crime_monitoring;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recycler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+    }
+}
